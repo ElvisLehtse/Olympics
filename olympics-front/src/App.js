90962419e0d41e717345321687569f5c8443fb77
ElvisLehtse <elehtse@gmail.com>
2024-07-16 17:07:27 +0300

    front-end: deleted useRef()'s; now using formData and payload

--- olympics-front/src/App.js
+++ olympics-front/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { useEffect, useRef, useState } from 'react';
-//test
+import { useEffect, useState } from 'react';
 
 function App() {
   const[responseAthlete, setResponseAthlete] = useState([]);
@@ -13,17 +12,6 @@ function App() {
   const[disciplines, setDisciplines] = useState([]);
   const[responseScore, setResponseScore] = useState("");
   const[disciplinesAndTotal, setDisciplinesAndTotal] = useState([]);
-  const firstNameRef = useRef();
-  const lastNameRef = useRef();
-  const ageRef = useRef();
-  const countryRef = useRef();
-  const getCountryFilterRef = useRef();
-  const setAthleteRef = useRef();
-  const getAthleteRef = useRef();
-  const getDisciplinesAndTotalRef = useRef();
-  const disciplineRef = useRef();
-  const resultRef = useRef();
-
 
   useEffect(() => {
     fetch("http://localhost:8080/countries")
@@ -43,11 +31,10 @@ function App() {
 
   function setAthlete(event) {
     event.preventDefault();
-    const firstName = firstNameRef.current.value;
-    const lastName = lastNameRef.current.value;
-    const age = ageRef.current.value;
-    const country = countryRef.current.value;
-    fetch("http://localhost:8080/registration?firstname=" + firstName + "&lastname=" + lastName + "&age=" + age + "&country=" + country, {method: "POST"}) 
+    const formData = new FormData(event.target);
+    const payload = Object.fromEntries(formData);
+    fetch("http://localhost:8080/registration?firstname=" + payload.firstname + "&lastname=" + 
+      payload.lastname+ "&age=" + payload.age + "&country=" + payload.country, {method: "POST"}) 
     .then(res => res.text())
     .then(body => {
       setResponseAthlete(body)
@@ -64,30 +51,31 @@ function App() {
 
   function setResult(event) {
     event.preventDefault();
-    const athleteUuid = setAthleteRef.current.value;
-    const discipline = disciplineRef.current.value;
-    const result = resultRef.current.value;
-    fetch("http://localhost:8080/setResults?athlete=" + athleteUuid + "&discipline=" + discipline + "&result=" + result, {method: "POST"})
+    const formData = new FormData(event.target);
+    const payload = Object.fromEntries(formData);
+    fetch("http://localhost:8080/setResults?athlete=" + payload.athlete + "&discipline=" + 
+      payload.discipline + "&result=" + payload.result, {method: "POST"})
     .then(res => res.text())
     .then(body => setResponseResult(body));
   }
 
   function getResults(event) {
     event.preventDefault();
-    const athleteUuid = getAthleteRef.current.value;
-    fetch("http://localhost:8080/getResults?athlete=" + athleteUuid)
+    const formData = new FormData(event.target);
+    const payload = Object.fromEntries(formData);
+    fetch("http://localhost:8080/getResults?athlete=" + payload.athlete)
     .then(res => res.json())
     .then(body => setAthleteResults(body));
-    fetch("http://localhost:8080/getScore?athlete=" + athleteUuid)
+    fetch("http://localhost:8080/getScore?athlete=" + payload.athlete)
     .then(res => res.json())
     .then(body => setResponseScore(body));
   }
 
   function getTopAthletes(event) {
     event.preventDefault();
-    const discipline = getDisciplinesAndTotalRef.current.value;
-    const country = getCountryFilterRef.current.value;
-    fetch("http://localhost:8080/getTopAthletes?discipline=" + discipline + "&country=" + country)
+    const formData = new FormData(event.target);
+    const payload = Object.fromEntries(formData);
+    fetch("http://localhost:8080/getTopAthletes?discipline=" + payload.total + "&country=" + payload.country)
     .then(res => res.json())
     .then(body => setTopAthletes(body));
   }
@@ -101,25 +89,25 @@ function App() {
           <div className="form-group">
             <label htmlFor="firstname">First name:</label>
             <div className="col-sm-5">
-              <input ref={firstNameRef} type="text" id="firstname" name="firstname" placeholder="first name" className="form-control"></input>
+              <input type="text" id="firstname" name="firstname" placeholder="first name" className="form-control"></input>
             </div>
           </div>
           <div className="form-group">
             <label htmlFor="lastname">Last name:</label>
             <div className="col-sm-5">
-              <input ref={lastNameRef} type="text" id="lastname" name="lastname" placeholder="last name" className="form-control"></input>
+              <input type="text" id="lastname" name="lastname" placeholder="last name" className="form-control"></input>
             </div>
           </div>
           <div className="form-group">
             <label htmlFor="age">Age:</label>
             <div className="col-sm-2">
-              <input ref={ageRef} type="number" id="age" name="age" placeholder="age" className="form-control"></input>
+              <input type="number" id="age" name="age" placeholder="age" className="form-control"></input>
             </div>
           </div>
           <div className="from-group">
             <div className="col-sm-2">
               <label htmlFor="country" className="form-label">Country</label>
-              <select ref={countryRef} className="form-select" name="country" id="country">
+              <select className="form-select" name="country" id="country">
                 {countries.map(country => <option key={country}>{country}</option>)}
               </select>
             </div>
@@ -133,15 +121,15 @@ function App() {
           <div className="form-group">
             <div className="col-sm-5">
               <label htmlFor="athlete" className="form-label">Select an athlete:</label>
-              <select ref={setAthleteRef} className="form-select" name="athlete" id="athlete">
-                {athletes.map(athlete => <option key={athlete} value={athlete.uuid}>{athlete.firstName + " " + athlete.lastName + " (" + athlete.country + ")"}</option>)}
+              <select className="form-select" name="athlete" id="athlete">
+                {athletes.map(athlete => <option key={athlete.uuid} value={athlete.uuid}>{athlete.firstName + " " + athlete.lastName + " (" + athlete.country + ")"}</option>)}
               </select>
             </div>
           </div>
           <div className="form-group">
             <div className="col-sm-5">
               <label htmlFor="discipline" className="form-label">Select a discipline:</label>
-              <select ref={disciplineRef} className="form-select" name="discipline" id="discipline">
+              <select className="form-select" name="discipline" id="discipline">
                 {disciplines.map(discipline => <option key={discipline}>{discipline}</option>)}
               </select>
             </div>
@@ -149,7 +137,7 @@ function App() {
           <div className="form-group">
             <label htmlFor="result">Result:</label>
             <div className="col-sm-2">
-              <input ref={resultRef} type="number" id="result" name="result" placeholder="result" className="form-control"></input>
+              <input type="number" id="result" name="result" placeholder="result" className="form-control"></input>
             </div>
           </div>
           <button type="submit" className="btn btn-primary mt-3"Submit>Submit</button>
@@ -161,7 +149,7 @@ function App() {
           <div className="form-group">
             <div className="col-sm-5">
               <label htmlFor="athlete" className="form-label">Select an athlete:</label>
-              <select ref={getAthleteRef} className="form-select" name="athlete" id="athlete">
+              <select className="form-select" name="athlete" id="athlete">
                 {athletes.map(athlete => <option key={athlete} value={athlete.uuid}>{athlete.firstName + " " + athlete.lastName + " (" + athlete.country + ")"}</option>)}
               </select>
             </div>
@@ -175,12 +163,13 @@ function App() {
           <span>Total score: {responseScore}</span>
         </div>
         }<br/>
+
         <form onSubmit={getTopAthletes}>
           <h5>View top Athletes</h5>
           <div className="form-group">
             <div className="col-sm-5">
               <label htmlFor="total" className="form-label">Sort by:</label>
-              <select ref={getDisciplinesAndTotalRef} className="form-select" name="total" id="total">
+              <select className="form-select" name="total" id="total">
                 {disciplinesAndTotal.map(total => <option key={total}>{total}</option>)}
               </select>
             </div>
@@ -188,7 +177,7 @@ function App() {
           <div className="from-group">
             <div className="col-sm-2">
               <label htmlFor="country" className="form-label">Filter by country</label>
-              <select ref={getCountryFilterRef} className="form-select" name="country" id="country">
+              <select className="form-select" name="country" id="country">
                 {countriesFilter.map(country => <option key={country}>{country}</option>)}
               </select>
             </div>
@@ -196,6 +185,7 @@ function App() {
           <button type="submit" className="btn btn-primary mt-3"Submit>Submit</button>
         </form>
         <br/>
+
         {topAthletes.length !== 0 &&
           <div>
             <br/><span>List of top Athletes for {topAthletes[0].discipline}:</span><br/>
